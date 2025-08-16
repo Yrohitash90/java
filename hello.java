743c042a4e13971a4f91fc9b2a14d66039b17827
rohtash <yrohitash90@gmail.com>
2025-08-16 15:24:32 +0530

    commit

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 public class hello{
     public static void main(String[] sfsdf){
         System.out.println("Hello ,world");
     }
+=======
+public class hello{
+    public static void main(String[] sfsdf){
+        System.out.println("hello world");
+    }
+>>>>>>> 70a45a4 (commit)
 }
