14c2dfcb4d19ef26279ac4c99709f1569f77bff9
rohtash <yrohitash90@gmail.com>
2025-08-18 10:26:52 +0530

    commit

--- hello_method.java
+++ hello_method.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
 public class hello_method{
@@ -15,4 +16,23 @@ public class hello_method{
         hello_method obj=new hello_method();
         obj.hello_print(n);
     }
+=======
+import java.util.Scanner;
+
+public class hello_method{
+
+    void hello_print(int n){
+        for(int i=0;i<n;i++){
+            System.out.println("Namaste India");
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("how many time u want to print namaste=");
+        int n=sc.nextInt();
+        hello_method obj=new hello_method();
+        obj.hello_print(n);
+    }
+>>>>>>> d060edb (commit)
 }
