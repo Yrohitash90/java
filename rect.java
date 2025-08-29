7c5bd00224f316a52bdaaff1d65c9284fd51c8d0
rohtash <yrohitash90@gmail.com>
2025-08-29 11:01:55 +0530

    updated

--- rect.java
+++ rect.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class rect1{
     int width,length;
     void getData(int x,int y){
@@ -17,4 +18,25 @@ public class rect{
         int area=r1.areaRect();
         System.out.println("area of rectangle="+area);
     }
+=======
+class rect1{
+    int width,length;
+    void getData(int x,int y){
+        width=x;
+        length=y;
+    }
+
+    int areaRect(){
+        return width*length;
+    }
+}
+
+public class rect{
+    public static void main(String[] args) {
+        rect1 r1=new rect1();
+        r1.getData(5, 4);
+        int area=r1.areaRect();
+        System.out.println("area of rectangle="+area);
+    }
+>>>>>>> 8932b02 (updated)
 }
